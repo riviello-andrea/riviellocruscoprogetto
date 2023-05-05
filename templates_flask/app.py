568d5d8dced146adca5d639a33e6637bc042c9b9
riviello-andrea <riviello.andrea@iisgalvanimi.edu.it>
2023-05-05 09:44:22 +0000

    v2

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -1,10 +1,21 @@
 from flask import render_template
 from flask import Flask
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="NYC"
+)
+
+mycursor = mydb.cursor()
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return render_template('hello.html', name='Tizi')
+def unitList():
+    mycursor.execute("SELECT * FROM Airbnb limit 17")
+    myresult=mycursor.fetchall()
+    return render_template('nycairbnb.html', units=myresult)
